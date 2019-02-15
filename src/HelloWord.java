13b28ef0cdee21401372de5c926a68d0fed0d0d6
Abrehet <Gualwediasfaha@gmail.com>
2019-02-15 09:48:11 -0500

    newOne

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("Good Evening");
+        System.out.println("Happy coding");
     }
 }
